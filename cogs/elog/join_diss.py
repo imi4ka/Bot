419050265bb62e24a9284945d72e5def5baebb35
imi4ka <filatrvvlad@gmail.com>
2024-08-09 04:13:42 +0400

    refactoring data base

--- cogs/elog/join_diss.py
+++ cogs/elog/join_diss.py
@@ -18,13 +18,12 @@ class Join_Diss(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member):
         guild = member.guild
-        global joined_at
         joined_at = member.joined_at.astimezone(self.moscow_tz).strftime('%Y-%m-%d %H:%M:%S')
-        result = int(db.get_channel_id())
-        if result:
-            db.update_members_join_at_user_id(joined_at, member.id)
+        member_id = [ids[1] for ids in db.get_members_ids()]
+        if str(member.id) in member_id:
+            db.update_members_join_at_user_id(member.name, member.id)
         else:
-            db.insert_members_join(member.id, joined_at, '')
+            db.insert_members_join(member.id, member.name)
         embed = disnake.Embed(color=disnake.Color.dark_gray(), title="", description=f"{member.mention} присоединился к серверу.")
         embed.set_author(name=member.name, icon_url=member.display_avatar.url)
         embed.set_thumbnail(url=member.display_avatar.url)
@@ -39,27 +38,22 @@ class Join_Diss(commands.Cog):
         moscow_tz = pytz.timezone('Europe/Moscow')
         left_at = datetime.now(tz=moscow_tz).strftime('%Y-%m-%d %H:%M:%S')
         guild = member.guild
-        if joined_at:
-            joined_at_dt = datetime.strptime(joined_at, '%Y-%m-%d %H:%M:%S').replace(tzinfo=moscow_tz)
-            left_at_dt = datetime.strptime(left_at, '%Y-%m-%d %H:%M:%S').replace(tzinfo=moscow_tz)
-            time_on_server = left_at_dt - joined_at_dt
-            days_on_server = time_on_server.days
-            hours_on_server = time_on_server.seconds // 3600
-            minutes_on_server = (time_on_server.seconds // 60) % 60
-            seconds_on_server = time_on_server.seconds % 60
-            db.update_members_left_at(left_at, member.id)
-            embed = disnake.Embed(color=disnake.Color.dark_gray(), title="", description=f"{member.mention} покинул сервер.")
-            embed.set_author(name=member.name, icon_url=member.display_avatar.url)
-            embed.set_thumbnail(url=member.display_avatar.url)
-            embed.add_field(name="Дата вступления на сервер", value=f"{joined_at}", inline=False)
-            embed.add_field(name="Время, проведенное на сервере:", value=f"{days_on_server} дней, {hours_on_server} часов, {minutes_on_server} минут, {seconds_on_server} секунд", inline=False)
-            embed.set_footer(text=f"{guild.name} • Дата выхода: {datetime.now(tz=moscow_tz).strftime('%B %d, %Y %H:%M')}")
-            channel = self.bot.get_channel(sinc_channel_id())
-            print(channel)
-            await channel.send(embed=embed)
-            db.delete_members_member_id(member.id)
-        else:
-            pass
+        joined_at = member.joined_at.astimezone(self.moscow_tz).strftime('%Y-%m-%d %H:%M:%S')
+        joined_at_dt = datetime.strptime(joined_at, '%Y-%m-%d %H:%M:%S').replace(tzinfo=moscow_tz)
+        left_at_dt = datetime.strptime(left_at, '%Y-%m-%d %H:%M:%S').replace(tzinfo=moscow_tz)
+        time_on_server = left_at_dt - joined_at_dt
+        days_on_server = time_on_server.days
+        hours_on_server = time_on_server.seconds // 3600
+        minutes_on_server = (time_on_server.seconds // 60) % 60
+        seconds_on_server = time_on_server.seconds % 60
+        embed = disnake.Embed(color=disnake.Color.dark_gray(), title="", description=f"{member.mention} покинул сервер.")
+        embed.set_author(name=member.name, icon_url=member.display_avatar.url)
+        embed.set_thumbnail(url=member.display_avatar.url)
+        embed.add_field(name="Дата вступления на сервер", value=f"{joined_at}", inline=False)
+        embed.add_field(name="Время, проведенное на сервере:", value=f"{days_on_server} дней, {hours_on_server} часов, {minutes_on_server} минут, {seconds_on_server} секунд", inline=False)
+        embed.set_footer(text=f"{guild.name} • Дата выхода: {datetime.now(tz=moscow_tz).strftime('%B %d, %Y %H:%M')}")
+        channel = self.bot.get_channel(sinc_channel_id())
+        await channel.send(embed=embed)
     
     @commands.Cog.listener()
     async def on_invite_create(self, invite: disnake.Invite):
--- database/db.py
+++ database/db.py
@@ -6,34 +6,39 @@ class Db:
     def __init__(self):
         self.connect = sqlite3.connect(self.dbname)
         self.create_tabel_settings()
-        self.create_tabel_join()
+        self.create_tabel_members()
         self.create_tabel_economy()
+        self.create_tabel_users_roles()
+        self.create_tabel_roles()
         
     def create_tabel_economy(self):
         c = self.connect.cursor()
         c.execute('''CREATE TABLE IF NOT EXISTS economy(member_id TEXT, level TEXT, exp TEXT, eternal_role_id TEXT, temporary_role_id TEXT, worker_role_id TEXT)''')
 
+    def create_tabel_roles(self):
+        c = self.connect.cursor()
+        c.execute('''CREATE TABLE IF NOT EXISTS roles(id INTEGER PRIMARY KEY AUTOINCREMENT, role_name TEXT, role_id TEXT, cost TEXT, limit_buying TEXT, description TEXT, reply_message TEXT, role_type TEXT)''')
+
+    def create_tabel_users_roles(self):
+        c = self.connect.cursor()
+        c.execute('''CREATE TABLE IF NOT EXISTS users_roles(user_id TEXT, role_id TEXT)''')
+
     def create_tabel_settings(self):
         c = self.connect.cursor()
         c.execute('''CREATE TABLE IF NOT EXISTS settings(channel_id TEXT)''')
         
-    def create_tabel_join(self):
+    def create_tabel_members(self):
         c = self.connect.cursor()
-        c.execute('''CREATE TABLE IF NOT EXISTS member_join(user_id INTEGER PRIMARY KEY, joined_at TEXT, left_at TEXT)''')
+        c.execute('''CREATE TABLE IF NOT EXISTS members(id INTEGER PRIMARY KEY AUTOINCREMENT, user_id TEXT, user_name TEXT, cash TEXT, bank TEXT, joined_at TEXT, left_at TEXT)''')
 
     def update_tabel_settings(self, channel_id):
         c = self.connect.cursor()
         c.execute("UPDATE settings SET channel_id = ?", (channel_id,))
         self.connect.commit()
 
-    def update_members_join_at_user_id(self, joined_at, member_id):
+    def update_members_join_at_user_id(self, user_name, member_id):
         c = self.connect.cursor()
-        c.execute("UPDATE member_join SET joined_at = ? WHERE user_id = ?", (joined_at, member_id))
-        self.connect.commit()
-
-    def update_members_left_at(self, left_at, member_id):
-        c = self.connect.cursor()
-        c.execute("UPDATE member_join SET left_at = ? WHERE user_id = ?", (left_at, member_id))
+        c.execute("UPDATE members SET user_name = ?, cash = ?, bank = ? WHERE user_id = ?", (user_name, '0', '0', member_id))
         self.connect.commit()
 
     def get_channel_id(self):
@@ -41,48 +46,86 @@ class Db:
         c.execute("SELECT channel_id FROM settings LIMIT 1")
         return c.fetchone()[0]
     
-    def get_user_id_member_join(self, member_id):
-        c = self.connect.cursor()
-        c.execute("SELECT * FROM member_join WHERE user_id = ?", (member_id,))
-        return c.fetchone()[0]
-    
     def get_economy_all_role_id(self):
         c = self.connect.cursor()
         c.execute("SELECT eternal_role_id, temporary_role_id, worker_role_id FROM economy")
         return c.fetchall()
 
-    def insert_members_join(self, member_id, joined_at):
+    def get_economy_eternal_role_id(self):
         c = self.connect.cursor()
-        c.execute("INSERT INTO member_join (user_id, joined_at, left_at) VALUES (?, ?, ?)", (member_id, joined_at, ''))
+        c.execute("SELECT eternal_role_id FROM economy")
+        return c.fetchall()
+    
+    def get_economy_temporary_role_id(self):
+        c = self.connect.cursor()
+        c.execute("SELECT temporary_role_id FROM economy")
+        return c.fetchall()
+    
+    def get_economy_worker_role_id(self):
+        c = self.connect.cursor()
+        c.execute("SELECT worker_role_id FROM economy")
+        return c.fetchall()
+    
+    def get_all_roles(self):
+        c = self.connect.cursor()
+        c.execute("SELECT * FROM roles")
+        return c.fetchall()
+    
+    def get_role_by_id(self, role_id):
+        c = self.connect.cursor()
+        c.execute("SELECT * FROM roles WHERE role_id = ?", (role_id,))
+        return c.fetchall()
+    
+    def get_all_without_roles(self):
+        c = self.connect.cursor()
+        c.execute("SELECT * FROM roles WHERE role_type IS NULL")
+        return c.fetchall()
+
+    def get_all_typed_roles(self, role_type):
+        c = self.connect.cursor()
+        c.execute("SELECT * FROM roles WHERE role_type = ?", (role_type,))
+        return c.fetchall()
+
+    def get_members_ids(self):
+        c = self.connect.cursor()
+        c.execute('''SELECT * FROM members''')
+        return c.fetchall()
+
+    def insert_members_join(self, member_id, member_name):
+        c = self.connect.cursor()
+        c.execute("INSERT INTO members (user_id, user_name, cash, bank) VALUES (?, ?, ?, ?)", (member_id, member_name, '0', '0'))
         self.connect.commit()
 
-    def insert_economy_eternal_role(self, roles_id):
+    def insert_typed_role(self, cost, limit_buying, description, reply_message, roles_id):
         c = self.connect.cursor()
-        c.execute("INSERT INTO economy (eternal_role_id) VALUES (?)", (roles_id,))
+        c.execute("INSERT INTO roles (cost, limit_buying, description, reply_message) VALUES (?, ?, ?, ?) WHERE roles_id = ?", (cost, limit_buying, description, reply_message, roles_id,))
         self.connect.commit()
 
-    def insert_economy_temporary_role(self, roles_id):
+    def insert_type_role(self, role_type, roles_id):
         c = self.connect.cursor()
-        c.execute("INSERT INTO economy (temporary_role_id) VALUES (?)", (roles_id,))
-        self.connect.commit()
-
-    def insert_economy_worker_role(self, roles_id):
-        c = self.connect.cursor()
-        c.execute("INSERT INTO economy (worker_role_id) VALUES (?)", (roles_id,))
-        self.connect.commit()
+        c.execute("UPDATE roles SET role_type = ? WHERE role_id = ?", (role_type, roles_id))
+        self.connect.commit()  
         
-    def delete_members_member_id(self, member_id):
+    def insert_role(self, role_name, role_id):
         c = self.connect.cursor()
-        c.execute("DELETE FROM member_join WHERE user_id = ?", (member_id,))
+        c.execute("INSERT INTO roles (role_name, role_id) VALUES (?, ?)", (role_name, role_id))
         self.connect.commit()
 
 
-# db = Db()
+
+
+
+db = Db()
 # # id = db.get_channel_id()
 # # print(id)
+# l = db.get_all_roles()
+# l.remove(1)
+# print(l)
 
-# list = db.get_economy_all_role_id()
-# for t in list:
-#     r = [t[0], t[1], t[2]]
-#     print(r)
+# # if db.get_economy_temporary_role_id() != None:
+# #     eternal_list = db.get_temporary_eternal_role_id()
+# #     list = [list for list in eternal_list if eternal_list != None]
+# # else:
+    
 
+# # print(list)
